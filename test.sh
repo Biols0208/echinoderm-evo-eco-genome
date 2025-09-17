94f1c675a14ac6dd3be64a3e73f3f337e362b712
Biols <50230939+Biols0208@users.noreply.github.com>
2025-09-17 17:08:23 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,7 +5,11 @@ Rscript PGLS.R analysis_config.yaml
 Rscript phylopath.R
 
 ## plot 
+
 ### Creates scatter plots with linear regression lines and 95% CI
 Rscript plot_lm.R -h
+#Rscript plot_lm.R --input genome_Phenotype.csv --xcol DNA --ycol genomesize --pgls true --tree-file ref.tree --species-col Species --outfile output.pdf --pgls-correlation auto --width 13 --height 6.5
+
 ###  Creates box plots or violin plots with overlaid points colored by groups
 Rscript plot_box.R -h
+#Rscript plot_box.R --input genome_Phenotype.csv --xcol Reproductive_Mode --ycol genomesize --groupcol group --outfile test.pdf --theme-style bw --density-plot --effect-size --export-stats
