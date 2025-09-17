2500ab1c8a3bd33fd710b656265dd8e9410b1872
Biols <50230939+Biols0208@users.noreply.github.com>
2025-09-17 13:18:06 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,4 +2,10 @@
 Rscript PGLS.R analysis_config.yaml
 
 ## phylopath Analysis
-Rscript phylopath.r
+Rscript phylopath.R
+
+## plot 
+### Creates scatter plots with linear regression lines and 95% CI
+Rscript plot_lm.R -h
+###  Creates box plots or violin plots with overlaid points colored by groups
+Rscript plot_box.R -h
