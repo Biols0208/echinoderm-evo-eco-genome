7ed3db4b4ca7648cc7fdbe8c706ce8c3d507dc0d
Biols <50230939+Biols0208@users.noreply.github.com>
2026-03-19 16:31:52 +0800

    Delete all_pipeline/stepl_genome_ass.sh

--- all_pipeline/stepl_genome_ass.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-## for genome assembly, by hifiasm-0.19.9
-# *.clean.fq.gz are Hi-c data
-# HCS.fastq.gz is Hi-Fi data
-hifiasm -o HSC.asm -t48 -z20 --h1 HS_R1.clean.fq.gz --h2 HS_R2.clean.fq.gz HCS.fastq.gz
